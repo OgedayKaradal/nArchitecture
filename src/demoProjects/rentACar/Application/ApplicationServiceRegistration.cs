7f5319d325025d443c4710195e997435cc6b9d97
Ögeday Karadal <ogedaykaradal@gmail.com>
2022-09-03 21:03:10 +0300

    3 September 2022 (1)

--- src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
+++ src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
@@ -3,6 +3,8 @@ using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using Application.Features.Brands.Rules;
+using FluentValidation;
+using Core.Application.Pipelines.Validation;
 
 namespace Application
 {
@@ -16,12 +18,12 @@ namespace Application
 
       services.AddScoped<BrandBusinessRules>();
 
-      //services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+      services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
       //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
       //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
       //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheRemovingBehavior<,>));
       //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-      //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
 
       return services;
 
--- src/demoProjects/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,18 +1,24 @@
 using System;
 using Application.Features.Brands.Commands.CreateBrand;
 using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Models;
 using AutoMapper;
+using Core.Persistence.Paging;
 using Domain.Entities.Concrete;
 
 namespace Application.Features.Brands.Profiles
 {
   public class MappingProfiles : Profile
   {
-    public MappingProfiles()
-    {
-      CreateMap<Brand, CreatedBrandDto>().ReverseMap();
-      CreateMap<Brand, CreateBrandCommand>().ReverseMap();
-    }
+        public MappingProfiles()
+        {
+            CreateMap<Brand, CreatedBrandDto>().ReverseMap();
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+            CreateMap<IPaginate<Brand>, BrandListModel>().ReverseMap();
+            CreateMap<Brand, BrandListDto>().ReverseMap();
+            CreateMap<Brand, BrandGetByIdDto>().ReverseMap();
+
+        }
   }
 }
 
